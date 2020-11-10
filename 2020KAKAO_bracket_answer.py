9fb0cad7bca82158d5a057a66ef7133be4021290
jisu <jsroh1013@gmail.com>
2020-11-10 14:59:53 +0900

    answer

---
+++ 2020KAKAO_bracket_answer.py
@@ -0,0 +1,50 @@
+def balanced_index(p):
+    count=0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if i == (len(p)-1):
+            return i
+        if count == 0:
+            return i
+
+def check_proper(p):
+    count = 0
+    for i in p:
+        if i == '(':
+            count += 1
+        else:
+            if count == 0:
+                return False
+            count -= 1
+    return True
+
+def soulution(p):
+    answer=''
+    if p == '':
+        return answer
+    p = p[1:-1]
+    index=balanced_index(p)
+    u=p[:(index+1)]
+    v=p[(index+1):]
+    if check_proper(u):
+        answer=u+soulution(v)
+    else:
+        answer='('
+        answer+=soulution(v)
+        answer+=')'
+        u=list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] == ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
+
+graph = list(input())
+result = soulution(graph)
+
+print(result)
