51d66f95a9fa870101d2806efc5bdc68aac5621c
Jisu Rho <61077498+jisu1013@users.noreply.github.com>
2021-05-19 16:47:38 +0900

    Update Baekjoon-10610.py

--- GreedyAlgorithm/Baekjoon-10610.py
+++ GreedyAlgorithm/Baekjoon-10610.py
@@ -1,1 +1,13 @@
 # 30 (실버5)
+N_input = list(str(input()))
+if '0' not in N_input:
+    print(-1)
+else:
+    N_list = list(map(int, N_input))
+    if sum(N_list) % 3 != 0:
+        print(-1)
+    else:
+        N_list.sort(reverse=True)
+        N_list_s = list(map(str, N_list))
+        result = ''.join(N_list_s)
+        print(int(result))
